ae40728956b2e75b9f149810025278686c1c151e
Stepan Riha <sriha@cpan.org>
2008-01-19 05:16:02 +0000

    Import of SRIHA/Class-DBI-Plugin-DeepAbstractSearchPager-0.02 from CPAN.
    gitpan-cpan-distribution: Class-DBI-Plugin-DeepAbstractSearchPager
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SRIHA/Class-DBI-Plugin-DeepAbstractSearchPager-0.02.tar.gz
    gitpan-cpan-author:       SRIHA
    gitpan-cpan-maturity:     released

--- lib/Class/DBI/Plugin/DeepAbstractSearchPager.pm
+++ lib/Class/DBI/Plugin/DeepAbstractSearchPager.pm
@@ -6,7 +6,7 @@ use Carp;
 use UNIVERSAL::require;
 use base qw( Data::Page Class::Data::Inheritable ); 
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # D::P inherits from Class::Accessor::Chained::Fast
 __PACKAGE__->mk_accessors( qw( where abstract_attr per_page page order_by _cdbi_app ) );
